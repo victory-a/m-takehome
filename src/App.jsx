5f6083263f4986dbd077f8b1a2f7122154c04790
victory-a <victoryasokomeh@gmail.com>
2022-12-12 14:39:43 +0100

    fix build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './app.module.scss';
 import Button from './components/FormElelments/Button';
-import SelectInput from './components/FormElelments/SelectInput';
-import TextInput from './components/FormElelments/Textinput';
+import SelectInput from './components/FormElelments/SelectInput.jsx';
+import TextInput from './components/FormElelments/Textinput.jsx';
 import ProductList from './components/ProductsList/ProductList.jsx';
 import { useProducts, fieldTypes, sortOptions } from './contexts/ProductsProvider';
 
